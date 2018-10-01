14d52fd4d31fd2d75ceead2b596146e3df6564bd
Pol Sole Navais <pol.sole.navais@plab-home.novalocal>
2018-10-01 11:33:04 +0200

    initial

--- scripts/hapmap_pca_projection.sh
+++ scripts/hapmap_pca_projection.sh
@@ -6,7 +6,11 @@ cd ~/HUNT_PCA/
 
 # This script depends on PLINK v2.00a1LM.
 
-../soft/plink2 --bfile /mnt/archive/genotypes/plink/X --read-freq pca_hapmap.afreq --score pca_hapmap.eigenvec.var 2 3 header-read no-mean-imputation variance-normalize --score-col-nums 5-14 --out pca_proj_mydata
+mkdir /mnt/work/hunt/pca
 
+for samples in /mnt/work/pol/HUNT_PCA/*_samples
+do
+../soft/plink2 --bed /mnt/archive/hunt/genotypes/plink/genotyped_PID106764.bed --fam /mnt/archive/hunt/genotypes/plink/genotyped_PID106764.fam --bim /mnt/work/pol/HUNT_PCA/modif_genotyped_PID106764.bim --score raw_data/pca_hapmap.eigenvec.var 2 3 header-read no-mean-imputation variance-standardize --score-col-nums 5-14 --out /mnt/work/hunt/pca/$(basename "$samples" _samples)_hapmap_pca
+done
 
 
