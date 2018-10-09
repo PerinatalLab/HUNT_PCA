8bb02cc745d62918f44aea0f89c2d344adcd0960
Pol Sole Navais <pol.sole.navais@plab-home.novalocal>
2018-10-09 11:53:31 +0200

    samples were not splitted

--- scripts/hapmap_pca_projection.sh
+++ scripts/hapmap_pca_projection.sh
@@ -11,7 +11,7 @@ mkdir /mnt/work/hunt/pca/hapmap_projected
 
 for samples in /mnt/work/pol/HUNT_PCA/*_samples
 do
-../soft/plink2 --bed /mnt/archive/hunt/genotypes/plink/genotyped_PID106764.bed --fam /mnt/archive/hunt/genotypes/plink/genotyped_PID106764.fam --bim /mnt/work/pol/HUNT_PCA/modif_genotyped_PID106764.bim --score raw_data/pca_hapmap.eigenvec.var 2 3 header-read no-mean-imputation variance-standardize --score-col-nums 5-14 --out /mnt/work/hunt/pca/hapmap_projected/$(basename "$samples" _samples)_hapmap_pca
+../soft/plink2 --bed /mnt/archive/hunt/genotypes/plink/genotyped_PID106764.bed --fam /mnt/archive/hunt/genotypes/plink/genotyped_PID106764.fam --bim /mnt/work/pol/HUNT_PCA/modif_genotyped_PID106764.bim --keep "$samples"--score raw_data/pca_hapmap.eigenvec.var 2 3 header-read no-mean-imputation variance-standardize --score-col-nums 5-14 --out /mnt/work/hunt/pca/hapmap_projected/$(basename "$samples" _samples)_hapmap_pca
 done
 
 
